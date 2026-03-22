daabfc452444331449888d401d0b7f68a578f067
KathrynWoest <kathryn.woest@gmail.com>
2026-03-21 17:47:30 -0700

    Create initial matching and check for perfect matching

--- analysis.py
+++ analysis.py
@@ -61,9 +61,46 @@ def build_preference_seller(graph):
             if v == max_value:
                 pref_sell.add_edge(k, node)
 
-    # return the preference-seller graph
-    return pref_sell
+    # return the preference-seller graph, buyers, and sellers
+    return pref_sell, buyers, sellers
 
 def analysis(graph):
-    ps_graph = build_preference_seller(graph)
+    ps_graph, buyers, sellers = build_preference_seller(graph)
     
+    # create initial matching
+    for buyer in buyers:
+        edges = ps_graph.edges(buyer)
+        for u, v in edges:
+            if u != buyer:
+                seller = u
+            else:
+                seller = v
+
+            if "matched" not in ps_graph.nodes[seller]:
+                ps_graph.nodes[seller]["matched"] = buyer
+                ps_graph.edges[(u, v)]["matched"] = True
+    
+    unmatched = False
+    starter = None
+    # check for unmatched nodes
+    for seller in sellers:
+        if "matched" not in ps_graph.nodes[seller]:
+            unmatched = True
+            starter = seller
+            break
+    
+    if unmatched:
+        print(starter)
+
+    # tried this, but i really need to start the augmented path search from the buyers, not sellers
+    bfs_path = [[starter]]
+    edges = ps_graph.edges(starter)
+    next = []
+    for u, v in edges:
+        if u != starter:
+            other = u
+        else:
+            other = v
+        next.append(other)
+    bfs_path.append(next)
+    print(bfs_path)
