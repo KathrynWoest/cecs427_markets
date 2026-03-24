37488fb4b630549073f92d3076c365f1e0e91eab
KathrynWoest <kathryn.woest@gmail.com>
2026-03-24 10:09:09 -0700

    Add error handling

--- analysis.py
+++ analysis.py
@@ -38,35 +38,40 @@ def build_preference_seller(graph):
     if len(sellers) != len(buyers):
         raise Exception(f"Program terminated because market analysis is unable to be calculated. There are more buyers than sellers or vice versa.")
     
-    # begin constructing the preference-seller graph by adding all the nodes to the new graph
-    pref_sell = nx.Graph()
-    pref_sell.add_nodes_from(graph.nodes)
-    
-    # go through each buyer and determine what they value every seller's node at (value - price)
-    for node in buyers:
-        node_values = {}
-
-        # find all the buyer's edges and add what they value the seller at to a dict, using the seller as the key
-        linked_edges = graph.edges(node)
-        for u, v in linked_edges:
-            if u != node:
-                seller = u
-            else:
-                seller = v
-            val = graph.edges[(u, v)]["valuation"] - graph.nodes[seller]["price"]
-            node_values[seller] = val
-
-        # determine which seller(s) they value the highest, and add the edge(s) to the preference-seller graph
-        max_value = max(node_values.values())
-        for k, v in node_values.items():
-            if v == max_value:
-                pref_sell.add_edge(k, node)
+    try:
+        # begin constructing the preference-seller graph by adding all the nodes to the new graph
+        pref_sell = nx.Graph()
+        pref_sell.add_nodes_from(graph.nodes)
         
-    for node in pref_sell.nodes:
-        pref_sell.nodes[node]["matched"] = False
+        # go through each buyer and determine what they value every seller's node at (value - price)
+        for node in buyers:
+            node_values = {}
 
-    for u, v in pref_sell.edges:
-        pref_sell.edges[u, v]["matched"] = False
+            # find all the buyer's edges and add what they value the seller at to a dict, using the seller as the key
+            linked_edges = graph.edges(node)
+            for u, v in linked_edges:
+                if u != node:
+                    seller = u
+                else:
+                    seller = v
+                val = graph.edges[(u, v)]["valuation"] - graph.nodes[seller]["price"]
+                node_values[seller] = val
+
+            # determine which seller(s) they value the highest, and add the edge(s) to the preference-seller graph
+            max_value = max(node_values.values())
+            for k, v in node_values.items():
+                if v == max_value:
+                    pref_sell.add_edge(k, node)
+            
+        # initialize all nodes and edges to not be matched
+        for node in pref_sell.nodes:
+            pref_sell.nodes[node]["matched"] = False
+
+        for u, v in pref_sell.edges:
+            pref_sell.edges[u, v]["matched"] = False
+
+    except Exception as e:
+        raise Exception("Program terminated due to an error in constructing the preference-seller graph. Error:", e)
 
     # return the preference-seller graph, buyers, and sellers
     return pref_sell, buyers, sellers
@@ -75,37 +80,37 @@ def build_preference_seller(graph):
 def reconstruct_path(ps_graph, parents, end):
     """Function that takes a BFS-search and reconstructs the augmented path
     Inputs: the dict that stores all the traversed nodes and their parents and the ending unmatched node to end the augmented path on
-    Returns: True"""
+    Returns: N/A"""
     
-    path = []
-    # while we haven't found the original unmatched node (the buyer)
-    while end is not None:
-        # add the current node, and load its parent
-        path.append(end)
-        end = parents[end]
-    
-    path.reverse()
+    try:
+        path = []
+        # while we haven't found the original unmatched node (the buyer)
+        while end is not None:
+            # add the current node, and load its parent
+            path.append(end)
+            end = parents[end]
+        
+        path.reverse()
 
-    # iterate through the augmented path, switching the matched/unmatched edges
+        # iterate through the augmented path, switching the matched/unmatched edges
+        for i in range(len(path) - 1):
+            edge = (path[i], path[i + 1])
+            if ps_graph.edges[edge].get("matched"):
+                ps_graph.edges[edge]["matched"] = False
+            else:
+                ps_graph.edges[edge]["matched"] = True
+        
+        # now reset all the nodes too
+        for node in ps_graph.nodes():
+            ps_graph.nodes[node]["matched"] = False
+        
+        for u, v in ps_graph.edges():
+            if ps_graph.edges[(u, v)].get("matched"):
+                ps_graph.nodes[u]["matched"] = True
+                ps_graph.nodes[v]["matched"] = True
 
-    for i in range(len(path) - 1):
-        edge = (path[i], path[i + 1])
-        if ps_graph.edges[edge].get("matched"):
-            ps_graph.edges[edge]["matched"] = False
-        else:
-            ps_graph.edges[edge]["matched"] = True
-    
-    # now reset all the nodes too
-    for node in ps_graph.nodes():
-        ps_graph.nodes[node]["matched"] = False
-    
-    for u, v in ps_graph.edges():
-        if ps_graph.edges[(u, v)].get("matched"):
-            ps_graph.nodes[u]["matched"] = True
-            ps_graph.nodes[v]["matched"] = True
-
-    
-    return True
+    except Exception as e:
+        raise Exception("Program terminated because the augmented path was unable to be applied. Error:", e)
 
 
 def bfs_search(ps_graph, starter, sellers):
@@ -119,63 +124,67 @@ def bfs_search(ps_graph, starter, sellers):
     parent_tracker = {starter: None}
     iterator = 0
     
-    # we know that the number of BFS layers will never be longer than the number of nodes, so set that to be the guaranteed cut-off
-    while iterator < len(ps_graph.nodes()):
-        # even step, add ALL edges not visited yet
-        if iterator % 2 == 0:
-            next = []
-            # for each node in the layer before the one we are currently constructing, find ALL their edges
-            for next_node in bfs_path[iterator]:
-                edges = ps_graph.edges(next_node)
-                for u, v in edges:
-                    if u != next_node:
-                        other = u
-                    else:
-                        other = v
-                    # if the node the edge is connecting to hasn't been visited, add to the traversal
-                    if not ps_graph.edges[(u, v)].get("matched", False) and other not in visited:
-                        next.append(other)
-                        visited.append(other)
-                        parent_tracker[other] = next_node
+    try:
+        # we know that the number of BFS layers will never be longer than the number of nodes, so set that to be the guaranteed cut-off
+        while iterator < len(ps_graph.nodes()):
+            # even step, add ALL edges not visited yet
+            if iterator % 2 == 0:
+                next = []
+                # for each node in the layer before the one we are currently constructing, find ALL their edges
+                for next_node in bfs_path[iterator]:
+                    edges = ps_graph.edges(next_node)
+                    for u, v in edges:
+                        if u != next_node:
+                            other = u
+                        else:
+                            other = v
+                        # if the node the edge is connecting to hasn't been visited, add to the traversal
+                        if not ps_graph.edges[(u, v)].get("matched", False) and other not in visited:
+                            next.append(other)
+                            visited.append(other)
+                            parent_tracker[other] = next_node
 
-                        # we are on an even layer. if we find a non-matched node at this point, it indicates an augmented path
-                        # call `reconstruct_path()` to find and return the augmented path
-                        if other in sellers and not ps_graph.nodes[other].get("matched", False):
-                            reconstruct_path(ps_graph, parent_tracker, other)
-                            return True, None
-            
-            # if we actually added new nodes to this layer, then add them and iterate to the next layer
-            if len(next) > 0:
-                bfs_path.append(next)
-                iterator += 1
-            # otherwise, break early and finish function
+                            # we are on an even layer. if we find a non-matched node at this point, it indicates an augmented path
+                            # call `reconstruct_path()` to find and return the augmented path
+                            if other in sellers and not ps_graph.nodes[other].get("matched", False):
+                                reconstruct_path(ps_graph, parent_tracker, other)
+                                return True, None
+                
+                # if we actually added new nodes to this layer, then add them and iterate to the next layer
+                if len(next) > 0:
+                    bfs_path.append(next)
+                    iterator += 1
+                # otherwise, break early and finish function
+                else:
+                    break
+
+            # odd step, add matched edge not visited yet
             else:
-                break
+                next = []
+                # for each node in the layer before the one we are currently constructing, find their matched edges
+                for next_node in bfs_path[iterator]:
+                    edges = ps_graph.edges(next_node)
+                    for u, v in edges:
+                        if u != next_node:
+                            other = u
+                        else:
+                            other = v
+                        # if the node the edge is connecting to hasn't been visited and is the matching edge, add to the traversal
+                        if ps_graph.edges[(u, v)].get("matched", False) and other not in visited:
+                            next.append(other)
+                            visited.append(other)
+                            parent_tracker[other] = next_node
 
-        # odd step, add matched edge not visited yet
-        else:
-            next = []
-            # for each node in the layer before the one we are currently constructing, find their matched edges
-            for next_node in bfs_path[iterator]:
-                edges = ps_graph.edges(next_node)
-                for u, v in edges:
-                    if u != next_node:
-                        other = u
-                    else:
-                        other = v
-                    # if the node the edge is connecting to hasn't been visited and is the matching edge, add to the traversal
-                    if ps_graph.edges[(u, v)].get("matched", False) and other not in visited:
-                        next.append(other)
-                        visited.append(other)
-                        parent_tracker[other] = next_node
+                # if we actually added new nodes to this layer, then add them and iterate to the next layer      
+                if len(next) > 0:
+                    bfs_path.append(next)
+                    iterator += 1
+                # otherwise, break early and finish function
+                else:
+                    break
 
-            # if we actually added new nodes to this layer, then add them and iterate to the next layer      
-            if len(next) > 0:
-                bfs_path.append(next)
-                iterator += 1
-            # otherwise, break early and finish function
-            else:
-                break
+    except Exception as e:
+        raise Exception("Program terminated because something went wrong while searching for an augmented path. Error:", e)
 
     # return false to indicate no augmented path was found and that prices should be increased in the sellers
     return False, visited   
@@ -186,60 +195,66 @@ def analysis(graph):
     Inputs: the user graph
     Returns: printed description of the perfect matching and a dict that tracks each step of the process"""
 
+    # as the graph will be changed throughout the process, make a copy of it for the analysis
+    graph = copy.deepcopy(graph)
     perfect_matching = False
     graph_tracker = []
 
-    while not perfect_matching:
-        # build the preference-seller graph
-        ps_graph, buyers, sellers = build_preference_seller(graph)
-        
-        # create initial matching by matching buyers with first seller they can connect to
-        for buyer in buyers:
-            buyer_matched = False
-            edges = ps_graph.edges(buyer)
-            for u, v in edges:
-                if u != buyer:
-                    seller = u
-                else:
-                    seller = v
-
-                if not ps_graph.nodes[seller].get("matched", False):
-                    ps_graph.nodes[seller]["matched"] = True
-                    ps_graph.nodes[buyer]["matched"] = True
-                    ps_graph.edges[(u, v)]["matched"] = True
-                    buyer_matched = True
-
-                if buyer_matched:
-                    break
-        
-        result = True
-        while result:
-            # check for unmatched buyers
-            unmatched = False
-            starter = None
-
-            for buyer in buyers:
-                if not ps_graph.nodes[buyer].get("matched", False):
-                    starter = buyer
-                    unmatched = True
-                    break
-
-            # if all the buyers are matched, we found the perfect matching!
-            if not unmatched:
-                perfect_matching = True
-                graph_tracker.append(copy.deepcopy(ps_graph))
-                break
+    try:
+        while not perfect_matching:
+            # build the preference-seller graph
+            ps_graph, buyers, sellers = build_preference_seller(graph)
             
-            # else, not all matched, so determine if an augmented path exists
-            result, visited = bfs_search(ps_graph, starter, sellers)
-        
-            # there's no augmented path, just up the prices of the constricted set sellers and repeat
-            if result == False:
-                graph_tracker.append(copy.deepcopy(ps_graph))
-                for seller in sellers:
-                    if seller in visited:
-                        graph.nodes[seller]["price"] += 1
-                break
+            # create initial matching by matching buyers with first seller they can connect to
+            for buyer in buyers:
+                buyer_matched = False
+                edges = ps_graph.edges(buyer)
+                for u, v in edges:
+                    if u != buyer:
+                        seller = u
+                    else:
+                        seller = v
+
+                    if not ps_graph.nodes[seller].get("matched", False):
+                        ps_graph.nodes[seller]["matched"] = True
+                        ps_graph.nodes[buyer]["matched"] = True
+                        ps_graph.edges[(u, v)]["matched"] = True
+                        buyer_matched = True
+
+                    if buyer_matched:
+                        break
+            
+            result = True
+            while result:
+                # check for unmatched buyers
+                unmatched = False
+                starter = None
+
+                for buyer in buyers:
+                    if not ps_graph.nodes[buyer].get("matched", False):
+                        starter = buyer
+                        unmatched = True
+                        break
+
+                # if all the buyers are matched, we found the perfect matching!
+                if not unmatched:
+                    perfect_matching = True
+                    graph_tracker.append(copy.deepcopy(ps_graph))
+                    break
+                
+                # else, not all matched, so determine if an augmented path exists
+                result, visited = bfs_search(ps_graph, starter, sellers)
+            
+                # there's no augmented path, just up the prices of the constricted set sellers and repeat
+                if result == False:
+                    graph_tracker.append(copy.deepcopy(ps_graph))
+                    for seller in sellers:
+                        if seller in visited:
+                            graph.nodes[seller]["price"] += 1
+                    break
+
+    except Exception as e:
+        raise Exception("Program terminated because something went wrong in completing the market analysis. Error:", e)
 
     # print formatted results
     print("-----")
